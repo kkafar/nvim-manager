20553893e77e9fa3ed4be5ea839b5c9f9e8bb21f
Kacper Kafara <kacperkafara@gmail.com>
2023-04-18 01:45:48 +0200

    Add TabManNew command

--- lua/nvim-tab-manager.lua
+++ lua/nvim-tab-manager.lua
@@ -1,7 +1,9 @@
 local M = {}
 
 function M.setup()
-  vim.api.nvim_create_user_command('KafaraTest', 'echo "It works"', {})
+  vim.api.nvim_create_user_command('TabManNew', function (input)
+    vim.cmd("tabnew") 
+  end, {})
 end
 
 M.name = "nvim-tab-manager"
