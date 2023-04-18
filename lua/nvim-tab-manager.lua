34fc0200be1a795e0394173efe9dc818135fecfa
Kacper Kafara <kacperkafara@gmail.com>
2023-04-18 10:55:26 +0200

    Add basic tab commands

--- lua/nvim-tab-manager.lua
+++ lua/nvim-tab-manager.lua
@@ -1,11 +1,25 @@
 local M = {}
 
 function M.setup()
-  vim.api.nvim_create_user_command('TabManNew', function (input)
+  vim.api.nvim_create_user_command('ManTabNew', function (input)
     vim.cmd("tabnew") 
   end, {})
-end
 
-M.name = "nvim-tab-manager"
+  vim.api.nvim_create_user_command('ManTabClose', function (input)
+    vim.cmd("tabclose") 
+  end, {})
+
+  vim.api.nvim_create_user_command('ManTabNext', function (input)
+    vim.cmd("tabnext") 
+  end, {})
+
+  vim.api.nvim_create_user_command('ManTabPrev', function (input)
+    vim.cmd("tabprev")
+  end, {})
+
+  vim.api.nvim_create_user_command('ManTabList', function (input)
+    vim.cmd("tabs")
+  end, {})
+end
 
 return M
